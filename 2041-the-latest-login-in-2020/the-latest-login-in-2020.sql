beb2b9d3faa052bcf6077e7c9f83d3a250027ab9
Akshay Damodar Prabhu <65062915+Akshayd-05@users.noreply.github.com>
2026-01-22 17:18:07 -0500

    Time: 652 ms (29.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select user_id, max(time_stamp) as last_stamp
+from Logins
+where time_stamp >= '2020-01-01 00:00:00' AND time_stamp < '2021-01-01 00:00:00'
+group by user_id;

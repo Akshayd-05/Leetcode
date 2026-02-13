9b1c6c3f16400845e607c7226a3f87994550482d
Akshay Damodar Prabhu <65062915+Akshayd-05@users.noreply.github.com>
2026-02-13 12:37:37 -0500

    Time: 1520 ms (23.81%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement below
 select name
-from SalesPerson
-where sales_id not in(select o.sales_id from orders o join Company c on o.com_id = c.com_id where c.name = 'RED');
+from SalesPerson 
+where sales_id not in(select o.sales_id from Orders o join Company c on o.com_id = c.com_id where c.name = 'RED');

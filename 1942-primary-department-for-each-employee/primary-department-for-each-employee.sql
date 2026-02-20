bd34191fd13c0aea44798e0ea47980b1fac6b612
Akshay Damodar Prabhu <65062915+Akshayd-05@users.noreply.github.com>
2026-02-20 16:57:08 -0500

    Time: 480 ms (94.12%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
-select distinct employee_id, department_id
+select employee_id, department_id
 from Employee
 where primary_flag = 'Y'
-UNION 
-select distinct employee_id, department_id
+UNION
+select employee_id, department_id
 from Employee
 group by employee_id
-having count(*) = 1;
+having count(*) = 1
+order by employee_id;

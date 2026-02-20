f0c6843844151b1077d448bfde5760cea6be042d
Akshay Damodar Prabhu <65062915+Akshayd-05@users.noreply.github.com>
2026-02-20 17:11:19 -0500

    Time: 530 ms (90.64%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-select employee_id,
-(case when employee_id % 2 != 0 AND name not like 'M%' THEN salary ELSE 0 END) as bonus
+select employee_id, 
+(case when employee_id % 2 != 0 and name not like 'M%' THEN salary ELSE 0 END) as bonus
 from Employees
-group by employee_id
 order by employee_id;
